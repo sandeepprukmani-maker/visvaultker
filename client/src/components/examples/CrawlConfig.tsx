66565bd5a5bbbf8ed2b854f43e56a58c4852467e
Sandeep <sandeep.p.rukmani@gmail.com>
2025-11-02 03:24:44 +0530

    ghj

--- client/src/components/examples/CrawlConfig.tsx
+++
@@ -1,9 +0,0 @@
-import { CrawlConfig } from "../crawl-config"
-
-export default function CrawlConfigExample() {
-  return (
-    <div className="p-6 max-w-2xl">
-      <CrawlConfig onStartCrawl={(url, depth) => console.log({ url, depth })} />
-    </div>
-  )
-}
