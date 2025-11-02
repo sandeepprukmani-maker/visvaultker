22408a96a2d1c7ea206898658c5c3d60e21a5c17
Sandeep <sandeep.p.rukmani@gmail.com>
2025-11-02 06:37:58 +0530

    ghj

--- client/src/components/examples/CommandInput.tsx
+++
@@ -1,9 +0,0 @@
-import { CommandInput } from "../command-input"
-
-export default function CommandInputExample() {
-  return (
-    <div className="p-6 max-w-4xl">
-      <CommandInput onExecute={(cmd) => console.log("Execute:", cmd)} />
-    </div>
-  )
-}
