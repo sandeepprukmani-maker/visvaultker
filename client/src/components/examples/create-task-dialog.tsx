884160766885441db56781ae9d6baeb43fa5590b
Sandeep <sandeep.p.rukmani@gmail.com>
2025-11-04 21:10:54 +0530

    erfgthjk

--- client/src/components/examples/create-task-dialog.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateTaskDialog } from "../create-task-dialog";
-
-export default function CreateTaskDialogExample() {
-  return (
-    <div className="p-4">
-      <CreateTaskDialog />
-    </div>
-  );
-}
