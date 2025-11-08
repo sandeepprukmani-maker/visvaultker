1f527c4dc79fa63f9c666a6facd19a17ff571f93
Sandeep <sandeep.p.rukmani@gmail.com>
2025-11-08 21:08:17 +0530

    version2

--- client/src/components/examples/BrowserPreview.tsx
+++
@@ -1,9 +0,0 @@
-import { BrowserPreview } from '../BrowserPreview';
-
-export default function BrowserPreviewExample() {
-  return (
-    <div className="h-screen p-4">
-      <BrowserPreview url="https://example.com" />
-    </div>
-  );
-}
