a9c29d4d56f1fcbf2cda2eba91e93b83d30d264b
Sandeep <sandeep.p.rukmani@gmail.com>
2025-12-06 13:41:50 +0530

    5678

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Browser Automation Agent using smolagents.
-Control your browser with natural language commands.
-"""
-
-from src.browser_agent import BrowserAgent, create_agent
-from src.browser_tools import get_all_browser_tools
-
-__all__ = ["BrowserAgent", "create_agent", "get_all_browser_tools"]
